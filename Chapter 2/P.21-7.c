79fca578eff075053556971ad9ac929532c28ff6
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-10-16 19:46:20 +0200

    Update and rename P.2.01.c to P.21-7.c

---
+++ Chapter 2/P.21-7.c
@@ -0,0 +1,51 @@
+/*
+Conversions. This set of problems involves converting a value in one unit to a value in another unit. Each program should prompt the user for a value in the specified units and then
+print the converted value, along with the new units.
+21. Write a program to convert miles to kilometers.
+22. Write a program to convert meters to miles.
+23. Write a program to convert pounds to kilograms.
+24. Write a program to convert newtons to pounds.
+25. Write a program that converts degrees Fahrenheit to degrees Rankin
+26. Write a program that converts degrees Celsius to degrees Rankin
+27. Write a program that converts degrees Kelvin to degrees Fahrenheit
+*/
+//21
+#include <stdio.h>
+
+int main() {
+    double miles, kilometers;
+
+    // Input the distance in miles
+    printf("Enter distance in miles: ");
+    scanf("%lf", &miles);
+
+    // Convert miles to kilometers
+    kilometers = miles * 1.60934;
+
+    // Display the result
+    printf("%.2lf miles is equal to %.2lf kilometers.\n", miles, kilometers);
+
+    return 0;
+}
+//22
+#include <stdio.h>
+int main(){
+    double meters, miles;
+
+    //Input the distance
+    printf("Enter distance in meters: ");
+    scanf("%lf", &meters);
+
+    miles = meters * 0.000621371;
+
+    printf("%.5f meters is equal to %.5f miles. \n", meters, miles);
+
+    return 0;
+
+}//end of main
+
+//23
+
+
+
+
